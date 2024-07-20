f6b36f8b5bc35c51e14045144f05d640634debda
Axl Aparicio <axelaparicior@gmail.com>
2024-07-20 15:50:42 -0300

    format

--- drinks/views.py
+++ drinks/views.py
@@ -1,15 +1,16 @@
-from django.http import JsonResponse
 from .models import Drink
 from .serializer import DrinkSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+
 def __get_drink_list():
     drinks = Drink.objects.all()
     serializer = DrinkSerializer(drinks, many=True)
 
     return Response({'drinks': serializer.data})
+
 
 def __add_drink(req_data):
     serializer = DrinkSerializer(data=req_data)
@@ -19,10 +20,12 @@ def __add_drink(req_data):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 def __get_drink_details(drink):
     serializer = DrinkSerializer(drink)
     return Response(serializer.data)
+
 
 def __update_drink(drink, req_data):
     serializer = DrinkSerializer(drink, data=req_data)
@@ -31,6 +34,7 @@ def __update_drink(drink, req_data):
         return Response(serializer.data)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 def __delete_drink(drink):
     drink.delete()
@@ -46,7 +50,8 @@ def drinks_list_requests(request, format=None):
             return __add_drink(request.data)
         case _:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def drink_requests(request, id, format=None):
     try:
@@ -63,5 +68,3 @@ def drink_requests(request, id, format=None):
             return __delete_drink(drink)
         case _:
             return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    
